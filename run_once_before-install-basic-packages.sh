6986fa181fa26304791e5ab3d7440bdae3e9f500
Markus Kohlmeyer <21080962+NeonGreenLemon@users.noreply.github.com>
2025-11-10 03:24:31 +0100

    add default package installation

---
+++ run_once_before-install-basic-packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+## install normal packages
+sudo pacman -S --needed --noconfirm extra/tmux
+sudo pacman -S --needed --noconfirm extra/httpie
+sudo pacman -S --needed --noconfirm extra/maven
+sudo pacman -S --needed --noconfirm extra/krita
+sudo pacman -S --needed --noconfirm extra/keepassxc
+
+## install AUR packages
+yay -S --needed --noconfirm tmux-plugin-manager
+yay -S --needed --noconfirm bitwig-studio
+
+## install mise
+mise install
