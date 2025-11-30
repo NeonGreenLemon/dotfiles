370f86b8a57862fcaa6edabd3db06b15ebd68aec
Markus Kohlmeyer <21080962+NeonGreenLemon@users.noreply.github.com>
2025-11-30 10:19:56 +0100

    add fcitx5 theme

--- run_once_after-init-system.sh
+++ run_once_after-init-system.sh
@@ -9,3 +9,8 @@ mise install
 ## generate locals
 sudo locale-gen
 
+## change theme of fcitx5
+git clone https://github.com/catppuccin/fcitx5.git ~/fcitx5Theme
+mkdir -p ~/.local/share/fcitx5/themes/
+cp -r ~/fcitx5Theme/src/* ~/.local/share/fcitx5/themes
+rm -rf ~/fcitx5Theme
