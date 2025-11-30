025f2dc4dabaa14734a6ab263107f93bb6e96bac
Markus Kohlmeyer <21080962+NeonGreenLemon@users.noreply.github.com>
2025-11-30 08:25:47 +0100

    add yazi install

--- run_once_before-install-basic-packages.sh
+++ run_once_before-install-basic-packages.sh
@@ -7,6 +7,7 @@ sudo pacman -S --needed --noconfirm extra/httpie
 sudo pacman -S --needed --noconfirm extra/maven
 sudo pacman -S --needed --noconfirm extra/krita
 sudo pacman -S --needed --noconfirm extra/keepassxc
+sudo pacman -S --needed --noconfirm extra/yazi
 
 ## install AUR packages
 yay -S --needed --noconfirm tmux-plugin-manager
